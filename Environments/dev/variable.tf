9d11718aadfc2e419eaff3cec2e79489d13333c9
Vviikarm P Jhala <VviikarmPJhala@gmail.com>
2026-08-09 20:06:59 +0530

    variable added

--- Environments/dev/variable.tf
+++ Environments/dev/variable.tf
@@ -1,3 +1,9 @@
-variable "pdev_rgs" {}
-variable "pdev_vnets" {}
-variable "pdev_subnets" {}
+variable "pdev_rgs" {
+    type= map(any)
+}
+variable "pdev_vnets" {
+    type= map(any)
+}
+variable "pdev_subnets" {
+    type=map(any)
+}
