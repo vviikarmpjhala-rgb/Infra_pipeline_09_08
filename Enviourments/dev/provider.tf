9c1ea129100ca9362366c93b2e4fb2960f8b7b91
Vviikarm P Jhala <VviikarmPJhala@gmail.com>
2026-08-09 11:17:40 +0530

    code is working

--- Enviourments/dev/provider.tf
+++ Enviourments/dev/provider.tf
@@ -1,11 +1,11 @@
 terraform {
-    required_providers {
-        azurerm = {
-           source = "hashicorp/azurerm"
-           version = "4.71.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.71.0"
     }
   }
 }
 provider "azurerm" {
-    features {}
+  features {}
 }
